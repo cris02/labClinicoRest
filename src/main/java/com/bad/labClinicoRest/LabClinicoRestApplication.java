15dc409c0d57217e2479bf590c840ab228f04e1f
Cristian Aguilar <cristian.aguilar@echotechs.net>
2022-04-19 22:19:50 -0600

    Configuracion Inicial del proyecto api lab clinico

---
+++ src/main/java/com/bad/labClinicoRest/LabClinicoRestApplication.java
@@ -0,0 +1,13 @@
+package com.bad.labClinicoRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabClinicoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabClinicoRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bad/labClinicoRest/LabClinicoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bad.labClinicoRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabClinicoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
