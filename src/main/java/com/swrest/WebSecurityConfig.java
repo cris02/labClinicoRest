9a7e36ce289a533115a0d0e71bf19f69d827a62d
Marcelo Molina <mr14015@ues.edu.sv>
2022-05-30 16:48:28 -0600

    Create WebSecurityConfig.java

---
+++ src/main/java/com/swrest/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.swrest;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/*
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+*/
+
+//@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig { //extends WebSecurityConfigurerAdapter
+	/*
+	@Autowired
+	private DataSource datasource;
+
+	@Override
+	protected UserDetailsService userDetailsService() {
+		// TODO Auto-generated method stub
+		return super.userDetailsService();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authProvider() {
+		DaoAuthenticationProvider authprovider = new DaoAuthenticationProvider();
+		authprovider.setUserDetailsService(userDetailsService());
+		authprovider.setPasswordEncoder(passwordEncoder());
+		return authprovider;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		//super.configure(web);
+		auth.authenticationProvider(authProvider());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(http);
+	}
+	*/
+}
